4202fd4d845ebb593ace027b39bf934b09c54ba3
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2024-01-07 23:12:49 -0300

    Recursos Usuário e endpont de getId

--- src/main/java/com/sistema/parkapi/service/UsuarioService.java
+++ src/main/java/com/sistema/parkapi/service/UsuarioService.java
@@ -4,8 +4,11 @@ import com.sistema.parkapi.entity.Usuario;
 import com.sistema.parkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,11 @@ public class UsuarioService {
     @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscaPorId(Long id) {
+        Optional<Usuario> obj = usuarioRepository.findById(id);
+        return obj.orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
     }
 }
--- src/main/java/com/sistema/parkapi/web/controller/UsuarioController.java
+++ src/main/java/com/sistema/parkapi/web/controller/UsuarioController.java
@@ -4,10 +4,7 @@ import com.sistema.parkapi.entity.Usuario;
 import com.sistema.parkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -26,6 +23,12 @@ public class UsuarioController {
                 .fromCurrentRequest().path("/{id}")
                 .buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscaPorId(id);
+        return ResponseEntity.ok().body(user);
     }
 
 }
