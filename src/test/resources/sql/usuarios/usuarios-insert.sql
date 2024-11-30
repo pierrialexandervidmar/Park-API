a5072d9c52bfa5299a056685853569c7911c016b
pierrialexandervidmar <pierri@hotmail.com.br>
2024-11-30 15:08:46 -0300

    Preparando o ambiente de teste

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS (id, username, password, role) VALUES (100, 'ana@email.com', '123456', 'ROLE_ADMIN');
+INSERT INTO USUARIOS (id, username, password, role) VALUES (101, 'carlos@email.com', 'abcdef', 'ROLE_CLIENTE');
+INSERT INTO USUARIOS (id, username, password, role) VALUES (102, 'maria@email.com', '123123', 'ROLE_CLIENTE');
