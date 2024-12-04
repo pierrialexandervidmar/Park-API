3cc354f2fa316d65d18e647a5e3106ea9fcc07a6
pierrialexandervidmar <pierri@hotmail.com.br>
2024-12-04 17:39:21 -0300

    commit

--- src/test/java/com/sistema/parkapi/UsuarioIT.java
+++ src/test/java/com/sistema/parkapi/UsuarioIT.java
@@ -1,5 +1,6 @@
 package com.sistema.parkapi;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -11,5 +12,8 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 public class UsuarioIT {
     @Autowired
     WebTestClient testClient;
+
+    @Test
+    public void
 
 }
