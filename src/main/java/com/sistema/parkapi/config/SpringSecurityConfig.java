ed7a244e775c30b0a733498e5a25cdd7843bc760
pierrialexandervidmar <pierri@hotmail.com.br>
2025-01-23 23:11:03 -0300

    Preparanto autenticacao JWT

---
+++ src/main/java/com/sistema/parkapi/config/SpringSecurityConfig.java
@@ -0,0 +1,78 @@
+package com.sistema.parkapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableMethodSecurity
+@EnableWebMvc
+@Configuration
+public class SpringSecurityConfig {
+
+    /**
+     * Configura o filtro de segurança para a aplicação, definindo regras para autenticação e autorização.
+     *
+     * Este metodo desativa a proteção CSRF, o login via formulário, e a autenticação HTTP básica.
+     * Ele também define as permissões de acesso para diferentes requisições HTTP:
+     *
+     * - A requisição POST para "api/v1/usuarios" será permitida para todos os usuários.
+     * - Todas as outras requisições exigem autenticação.
+     *
+     * Além disso, a política de criação de sessões é configurada para **stateless**, o que significa que a aplicação
+     * não manterá informações sobre o estado da sessão entre as requisições.
+     *
+     * @param http Configuração do HttpSecurity para personalizar a segurança HTTP.
+     * @return A configuração do filtro de segurança, incluindo regras de autorização e gestão de sessão.
+     * @throws Exception Se ocorrer um erro durante a configuração do filtro de segurança.
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "api/v1/usuarios").permitAll()
+                        .anyRequest().authenticated()
+                ).sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).build();
+    }
+
+    /**
+     * Retorna um codificador de senha (PasswordEncoder) usando o algoritmo BCrypt.
+     *
+     * O BCrypt é um algoritmo de hash seguro e amplamente utilizado para armazenar senhas de forma criptografada.
+     * Ele aplica uma função de hash e um sal (salt) para gerar o hash da senha, dificultando ataques de força bruta.
+     *
+     * @return O codificador de senha baseado no algoritmo BCrypt.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Retorna o gerenciador de autenticação (AuthenticationManager) usado pelo Spring Security para autenticar os usuários.
+     *
+     * O AuthenticationManager é um componente essencial para o Spring Security,
+     * responsável por verificar as credenciais do usuário e determinar se ele está autorizado a acessar recursos protegidos.
+     *
+     * @param authenticationConfiguration Configuração de autenticação do Spring Security.
+     * @return O AuthenticationManager configurado.
+     * @throws Exception Se ocorrer um erro ao obter o AuthenticationManager.
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/sistema/parkapi/jwt/JwtUtils.java
@@ -0,0 +1,38 @@
+package com.sistema.parkapi.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Slf4j
+public class JwtUtils {
+
+    public static final String JWT_BEARER = "Bearer ";
+    public static final String JWT_AUTHORIZATION = "Authorization";
+    public static final String SECRET_KEY = "3f8a7b9d6e4c1e9d2c9a8f6e2b3d7c6a4f5a9b3d2e7f1c6";
+    public static final long EXPIRE_DAYS = 0;
+    public static final long EXPIRE_HOURS = 0;
+    public static final long EXPIRE_MINUTES = 2;
+
+    private JwtUtils() {
+    }
+
+    private static Key generateKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static Date toExpireDate(Date start) {
+        LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime end = dateTime.plusDays(EXPIRE_DAYS).plusHours(EXPIRE_HOURS).plusMinutes(EXPIRE_MINUTES);
+        return Date.from(end.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+
+}
